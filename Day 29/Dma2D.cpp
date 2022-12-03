4bb67efb9c47ca72db6edbce69371158c44640b4
Shubham Yadav <100077254+Shubham185y@users.noreply.github.com>
2022-12-03 16:33:39 +0530

    Day 31

--- Day 29/Dma2D.cpp
+++ Day 29/Dma2D.cpp
@@ -1,8 +1,5 @@
 #include<iostream>
 using namespace std;
-
-
-
 int main(){
      int row,col;
      cin>>row;
---
+++ Day 31/Recursion1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+
+int fact(int n){
+    int ans;
+    return n==0 ?  1 : n * fact(n-1); 
+//     if(n==0){
+//         return 1;
+//     }
+//     else{
+//          ans = n * fact(n-1);
+//     }
+//    return ans;
+}
+
+int count(int n){
+    if(n==0){
+        return 1;
+    }
+    else{
+        count(n-1);
+        cout<<n<<endl;
+    }
+}
+
+int main(){
+
+int n;
+cin>>n;
+//  cout << fact(n)<<endl;
+count(n);
+    return 0;
+}
