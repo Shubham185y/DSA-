f66c47d3d87d3d1c937879d3a45b9647e38e1ab7
Shubham Yadav <100077254+Shubham185y@users.noreply.github.com>
2022-12-19 01:30:36 +0530

    palindrome in string

--- Day 34/Stringrec.cpp
+++ Day 34/Stringrec.cpp
@@ -1,24 +1,47 @@
 #include<iostream>
 using namespace std;
+
+bool checkpalindrome(int i, int j , string str){
+    if(i>j){
+        return true;
+    }
+    if(str[i] != str[j]){
+        return false;
+    }
+    else{
+        return checkpalindrome(i++,j--,str);
+    }
+}
+
+
+
+
+
 void rev(int i, int j, string& s){
     if(i>j){
         return;
     }
-    
+    else{
         swap(s[i], s[j]);
         i++;
         j--;
         rev(i,j,s);
-    
+    }
 }
 
 int main(){
  
-string name ="shubham";
+string name ="madam";
 
 rev(0,name.length()-1,name);
 cout<<name<<endl;
-
+bool ispalindrome = checkpalindrome(0,name.length()-1,name);
+if(ispalindrome){
+    cout<<"It is palindrome"<<endl;
+}
+else{
+    cout<<"It is not a palindrome"<<endl;
+}
 
 
 
