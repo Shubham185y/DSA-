f996214b806fe61c087d1ed7ff18b33e2471dbba
Shubham Yadav <100077254+Shubham185y@users.noreply.github.com>
2023-01-18 22:42:15 +0530

    Day 60

---
+++ Day 60/Queue.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    queue<int> q;
+    /*q.push(12);
+    q.push(3);
+    cout<<q.size()<<endl;
+    cout<<q.front()<<endl;*/
+    
+    return 0;
+}
